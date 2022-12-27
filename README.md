f27eca85cac2e14256117f257a7d3e1720722a08
timeades <tim.eades@mac.com>
2022-12-27 22:19:47 +0000

    add basic functionality

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GoogleMaps-clone
+
+WIP
+
+using mapbox to build this clone. It's still a work in progress and more functionality will be added.
+
+Currently you can:
+1. Allow your current location to center the map at a zoom level of 15
+2. Controls to zoom in and out of the map as well as rotate
+3. Use the directions to plan an A to B journey either driving, walking or cycling.
---
+++ script.js
@@ -0,0 +1,31 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWR2dGltIiwiYSI6ImNsYzZxOTc2eDBya2wzb3Q3bmFwZnBzNmgifQ.DHnNsbbMud7MfAp-Qfmy7A';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+})
+
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+    setupMap([-2.24, 53.48])
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: center,
+        zoom: 15
+    })
+
+    const nav = new mapboxgl.NavigationControl()
+    map.addControl(nav)
+
+    const directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    })
+
+    map.addControl(directions, "top-left")
+}
